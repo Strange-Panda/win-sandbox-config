5ccf582ad942af03f9d4fa63cb57c4db9b7adea5
Chris <christoph@reit-it.de>
2022-11-08 00:13:59 +0100

    Create autostart.cmd

---
+++ scripts/setups/autostart.cmd
@@ -0,0 +1,12 @@
+set SETUP_PATH=C:\users\WDAGUtilityAccount\Desktop\scripts\setups
+rem we need a writeable location to launch MSIs it seems
+copy /B /Y /V %SETUP_PATH%\* %TEMP%\
+"%TEMP%\vscode.exe" /verysilent /suppressmsgboxes /MERGETASKS="!runcode,desktopicon,quicklaunchicon,addcontextmenufiles,addcontextmenufolders,addtopath"
+"%TEMP%\7zip.exe" /S
+"%TEMP%\notepad++.exe" /S
+"%TEMP%\vcredist_x64.exe"  /q /norestart
+"%PROGRAMFILES%\7-Zip\7z.exe" x -aoa "%TEMP%\sysinternals.zip" -o "%USERPROFILE%\Desktop\sysinternals"
+"%TEMP%\vcredist_x64.exe" /passive /norestart
+"%TEMP%\wireshark.exe" /S /desktopicon=yes
+"%TEMP%\npcap.exe" /loopback_support=yes
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
